dc1b74b61a8f4cd9d502f28049fbd5af3fce468f
mhernanda <mhernanda66@gmail.com>
2021-02-19 05:54:15 +0700

    Add object calculator and function updateDisplay for updating display and clearCalcultor for reset calculator

---
+++ assets/kalkulator.js
@@ -0,0 +1,17 @@
+const calculator = {
+    displayNumber : '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+};
+
+function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
